1966eb401019103900e70b6ed2f6ac2876037872
Ian Truslove <ian.truslove@sonian.net>
2014-11-20 21:16:29 +0000

    Add Alchemist for Elixir

--- lisp/init-erlang.el
+++ lisp/init-erlang.el
@@ -1,7 +1,11 @@
 (ignore-errors
-  (require-package 'erlang))
+  (require-package 'erlang)
+  (require-package 'alchemist))
 
 (when (package-installed-p 'erlang)
   (require 'erlang-start))
+
+(when (package-installed-p 'alchemist)
+  (setq alchemist-buffer-status-modeline nil))
 
 (provide 'init-erlang)
